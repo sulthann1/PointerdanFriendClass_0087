b9d33b79ae55d42f3176962640d92c8819f3fe81
sulthann1 <sulthanaawaliya155@gmail.com>
2025-05-06 15:34:02 +0700

    mengatur private dan public pada akses modfier

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -2,3 +2,24 @@
 #include <string>
 using namespace std;
 
+class siswa;
+class orang;
+{
+    private :
+    string nama;
+
+    public :
+        void setNama (string pNama);
+        friend class siswa;
+};
+
+class siswa 
+{
+    private :
+    int id;
+
+    public :
+    void setId (int pId);
+    void display All (orang &a);
+    
+}
