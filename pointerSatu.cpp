85b276c8df13f582721b9893138348fc3096c005
sulthann1 <sulthanaawaliya155@gmail.com>
2025-05-06 15:21:45 +0700

    menambahkan member pointer

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -1,1 +1,22 @@
 #include <iostream>
+using namespace std;
+
+class mahasiswa{
+    public :
+    int nim;
+    void showNim()
+    {
+        cout << "No Induk ="<<nim <<endl;
+    }
+};
+
+int main (){
+    mahasiswa mhs{1}; //objek mhs
+    mhs.showNim(); //member acces operator
+
+    mahasiswa &refMhs = mhs;// pointer reference refmhs
+    refMhs.nim = 2; //member acces oprator
+    mhs.showNim();
+    mahasiswa * pMhs; //pointer reference refMhs
+    pMhs -> nim = 3; //arrow operator 
+}
